5feeb063ae6f7e46a029e222877659d5afce62ea
LazyDuck927 <mikhael.tjokrodiponto@binus.ac.id>
2025-09-05 22:15:03 +0700

    Noice

---
+++ Sesi_3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("My Name Is Mikhael Dylan Tjokrodiponto\n");
+    printf("My Favorite Programming Language Is C\n");
+    
+    return 0;
+
+}                                            
